12c01056c5e5b0f93cb44c068d5c0fda51e286d5
frank <frankcool321@gmail.com>
2023-01-15 11:40:20 +0800

    feat: add populate method for records

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,12 +4,12 @@ const dayjs = require("dayjs"); //- 處理日期格式套件
 
 router.get("/", async (req, res) => {
   try {
-    const records = await Record.find().lean();
+    const records = await Record.find().populate("categoryId").lean();
     //- 處理日期格式
     records.forEach((record) => {
       record.date = dayjs(record.date).format("YYYY-MM-DD");
     });
-    console.log(records);
+    // console.log(records);
     return res.render("index", { records });
   } catch (err) {
     return res.render("error", { err });
--- routes/modules/record.js
+++ routes/modules/record.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Record = require("../../models/records");
+const Category = require("../../models/categories");
 
 router.get("/new", (req, res) => {
   return res.render("new");
@@ -8,7 +9,37 @@ router.get("/new", (req, res) => {
 router.post("/new", async (req, res) => {
   try {
     const { name, date, amount, category } = req.body;
-    const createdRecord = await Record.create(req.body);
+    //- 查詢和建立category
+    let foundCategory = await Category.findOne({ name: category });
+    if (!foundCategory) {
+      //- 如果沒有建立過此種類
+      let icon = "";
+      switch (category) {
+        case "home":
+          icon = "fa-solid fa-house";
+          break;
+        case "transport":
+          icon = "fa-solid fa-van-shuttle";
+          break;
+        case "entertainment":
+          icon = "fa-solid fa-face-grin-beam";
+          break;
+        case "food":
+          icon = "fa-solid fa-utensils";
+          break;
+        case "other":
+          icon = "fa-solid fa-pen";
+          break;
+      }
+      foundCategory = await Category.create({ name: category, icon });
+    }
+    const categoryId = foundCategory._id;
+    await Record.create({
+      name,
+      date,
+      amount,
+      categoryId,
+    });
     return res.redirect("/");
   } catch (err) {
     return res.render("error", { err });
