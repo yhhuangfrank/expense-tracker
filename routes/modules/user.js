86ed2de96279c4cd22f7b3cf494f91635b6b33e8
FrankHuang <frankcool321@gmail.com>
2023-01-21 16:58:28 +0800

    feat: modify login/register page and set corresponding initial route

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
+router.post("/login", () => {});
+
 router.get("/register", (req, res) => {
   return res.render("register");
 });
+
+router.post("/register", (req, res) => {});
 
 module.exports = router;
