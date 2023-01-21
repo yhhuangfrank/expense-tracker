a0b1eb6ea57429e8287fa65b482ce900e156967b
FrankHuang <frankcool321@gmail.com>
2023-01-21 20:14:38 +0800

    feat: add register function

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const User = require("../../models/users");
 
 router.post("/login", () => {});
 
@@ -6,6 +7,22 @@ router.get("/register", (req, res) => {
   return res.render("register");
 });
 
-router.post("/register", (req, res) => {});
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body;
+    const foundUser = await User.findOne({ email });
+    if (foundUser) {
+      console.log("此用戶已註冊過!");
+      return res.render("register", { name, email, password, confirmPassword });
+    } else {
+      //- 建立新用戶
+      await User.create({ name, email, password });
+      return res.redirect("/");
+    }
+  } catch (err) {
+    console.log(err);
+    return res.render("error", { err });
+  }
+});
 
 module.exports = router;
