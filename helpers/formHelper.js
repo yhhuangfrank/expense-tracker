bb20e549c0c6f6749924a25c9af15e9020fe8c32
FrankHuang <frankcool321@gmail.com>
2023-02-14 12:05:23 +0800

    refactor: modify checkForm

--- helpers/formHelper.js
+++ helpers/formHelper.js
@@ -2,15 +2,18 @@ module.exports = {
   checkForm: (data) => {
     const { name, date, amount, category } = data;
     const errMessage = [];
-    if (!name || !date || !amount || !category) {
+    if (!name || !date || !category) {
       errMessage.push({ message: "所有欄位皆為必填!" });
     }
     if (amount < 0) {
       errMessage.push({ message: "金額不可小於0" });
     }
-    //- 將JS雙精度範圍最大整數(2^53-1)設定為上限
-    if (amount > Number.MAX_SAFE_INTEGER) {
-      errMessage.push({ message: "輸入金額超過上限!" });
+    if (/\<script\>/.test(name)) {
+      errMessage.push({ message: "名稱格式有誤!" });
+    }
+    //- 將JS最大整數設定為上限
+    if (amount > Number.MAX_VALUE || amount === "") {
+      errMessage.push({ message: "輸入金額超過上限或為空!" });
     }
     return errMessage;
   },
